d7d71dee45b590d87b256cf096814589fe0c383a
Eric Tierra <91515255+ericstierra@users.noreply.github.com>
2023-05-02 16:52:34 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ $(document).ready(function () {
     var city;
     var name;
 
-    // Initial message
     $(".chatbot-messages").append("<div class='message'><p>Hello! I'm Sambag, your weather forecasting chatbot. What's your name?</p></div>");
 
-    // When user submits their name
     $("#name-submit").click(function () {
         name = $("#name-input").val();
         if (name != "") {
@@ -14,83 +12,56 @@ $(document).ready(function () {
         }
     });
 
-    // Ask for city
     function askCity() {
-
-        $(".chatbot-messages").append("<div class='message'><p>Which city would you like to know the weather for?</p></div>");
+        $(".chatbot-messages").append("<div class='message'><p>How can I help you today? You can type <span class='description'>'/weathernow city'</span> to get the current weather, or <span class='description'>'/forecast city'</span> to get the forecast for the next three days. What city are you interested in?</p></div>");
         $(".chatbot-input").html("<input type='text' placeholder='Enter a city' id='city-input'><button id='city-submit'>Submit</button>");
-
     }
 
-    function updateTime() {
-        const date = new Date();
-        const pstOffset = -8 * 60; // PST is 8 hours behind UTC
-        const pstDate = new Date(date.getTime() + pstOffset * 60 * 1000);
-        const timeString = pstDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-        document.getElementById('time-display').textContent = `PST: ${timeString}`;
-    }
-
-    // call updateTime every second to update the time display
-    setInterval(updateTime, 1000);
-
-    // Attach event listener for the city submit button
     $(document).on("click", "#city-submit", function () {
         city = $("#city-input").val();
         if (city != "") {
-            $(".chatbot-messages").append("<div class='message'><p>Let me check the weather for " + city + ".</p></div>");
-            getWeather(city);
+            var inputText = $("#city-input").val().toLowerCase().trim();
+            if (inputText.startsWith("/forecast ")) {
+                var cityName = inputText.replace("/forecast ", "");
+                $(".chatbot-messages").append("<div class='message'><p>Let me check the forecast for " + cityName + ".</p></div>");
+                getForecast(cityName);
+            } else if (inputText.startsWith("/weathernow ")) {
+                var cityName = inputText.replace("/weathernow ", "");
+                $(".chatbot-messages").append("<div class='message'><p>Let me check the weather for " + cityName + ".</p></div>");
+                getWeather(cityName);
+            } else {
+                $(".chatbot-messages").append("<div class='message'><p>Sorry, I didn't understand. Please enter either <span class='description'>'/forecast' or '/weathernow'</span> followed by a city name to get the weather or forecast.</p></div>");
+            }
         }
     });
 
-    // Get weather information from API
     function getWeather(city) {
-        // Your API key
         var apiKey = "3b903020d6e2acd2ce742ca91c71dfa1";
-        // API URL with city and API key
         var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
-        // Make API request
         $.get(apiUrl, function (data) {
-            // Weather information
             var description = data.weather[0].description;
             var temp = data.main.temp;
             var feelsLike = data.main.feels_like;
+            var windSpeed = data.wind.speed * 3.6; // Convert wind speed from m/s to km/h
+            var precipitation = data.rain ? data.rain["1h"] : 0;
 
-            // Convert temperature to Celsius
             temp = Math.round(temp - 273.15);
             feelsLike = Math.round(feelsLike - 273.15);
+            windSpeed = Math.round(windSpeed);
+            precipitation = Math.round(precipitation);
 
-            // Display weather information
-            $(".chatbot-messages").append("<div class='message'><p>The weather in " + city + " is <span class='description'>" + description + "</span> with a temperature of <span class='temp'>" + temp + "°C</span>. It feels like <span class='feels-like'>" + feelsLike + "°C</span>.</p></div>");
-
-            // Ask if user wants to search for another location
+            $(".chatbot-messages").append("<div class='message'><p>The weather in " + city + " is <span class='description'>" + description + "</span> with a temperature of <span class='temp'>" + temp + "°C</span>. It feels like <span class='feels-like'>" + feelsLike + "°C</span>. The wind speed is <span class='wind-speed'>" + windSpeed + " km/h</span>. The chance of precipitation in the last hour is <span class='precipitation'>" + precipitation + " mm</span>.</p></div>");
             $(".chatbot-messages").append("<div class='message'><p>Do you want to search for weather in another location?</p></div>");
-
-            // Show the input container
             $(".chatbot-input").show();
-
-            // Append the buttons
             $(".chatbot-input").html("<button id='yes'>Yes</button><button id='no'>No</button>");
 
-            // When user clicks "Yes"
             $("#yes").click(function () {
                 $(".chatbot-messages").append("<div class='message'><p>Okay, which city would you like to know the weather for?</p></div>");
                 $(".chatbot-input").html("<input type='text' placeholder='Enter a city' id='city-input'><button id='city-submit'>Submit</button>");
-
-                // Clear previous click events from the submit button
                 $("#city-submit").off("click");
-
-                /* // When user submits a city
-                 $("#city-submit").click(function () {
-                     city = $("#city-input").val();
-                     if (city != "") {
-                         $(".chatbot-messages").append("<div class='message'><p>Let me check the weather for " + city + ".</p></div>");
-                         getWeather(city);
-                     }
-                 }); */
             });
 
-            // When user clicks "No"
             $("#no").click(function () {
                 $(".chatbot-messages").append("<div class='message'><p>Okay, thanks for using Sambag, your weather forecasting chatbot. Have a great day!</p></div>");
                 $(".chatbot-input").html("");
@@ -98,4 +69,48 @@ $(document).ready(function () {
 
         });
     }
+
+
+    function getForecast(city) {
+        var apiKey = "3b903020d6e2acd2ce742ca91c71dfa1";
+        var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+
+        $.get(apiUrl, function (data) {
+            var forecast = data.list.slice(0, 3); // Get the first three items in the list, which represent the next three days
+            var city = data.city.name;
+
+            $(".chatbot-messages").append("<div class='message'><p>The weather forecast for " + city + " is:</p></div>");
+            forecast.forEach(function (item) {
+                var date = new Date(item.dt * 1000);
+                var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZone: 'Asia/Manila' };
+                var formattedDate = date.toLocaleString('en-US', options);
+                var description = item.weather[0].description;
+                var temp = Math.round(item.main.temp - 273.15);
+                var feelsLike = Math.round(item.main.feels_like - 273.15);
+
+                $(".chatbot-messages").append("<div class='message'><p>" + formattedDate + ": <span class='description'>" + description + "</span> with a temperature of <span class='temp'>" + temp + "°C</span>. It feels like <span class='feels-like'>" + feelsLike + "°C</span>.</p></div>");
+            });
+
+            $(".chatbot-messages").append("<div class='message'><p>Do you want to search for weather in another location?</p></div>");
+            $(".chatbot-input").show();
+            $(".chatbot-input").html("<button id='yes'>Yes</button><button id='no'>No</button>");
+
+            $("#yes").click(function () {
+                $(".chatbot-messages").append("<div class='message'><p>Okay, which city would you like to know the weather for?</p></div>");
+                $(".chatbot-input").html("<input type='text' placeholder='Enter a city' id='city-input'><button id='city-submit'>Submit</button>");
+                $("#city-submit").off("click");
+            });
+
+            $("#no").click(function () {
+                $(".chatbot-messages").append("<div class='message'><p>Okay, thanks for using Sambag, your weather forecasting chatbot. Have a great day!</p></div>");
+                $(".chatbot-input").html("");
+            });
+
+        });
+    }
+
+
+
+
+
 });
